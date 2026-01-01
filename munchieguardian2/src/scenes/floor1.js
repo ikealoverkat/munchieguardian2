fdbb8c7f2d46db93252fcb6162f324264ad646c4
kat <katwang7749@gmail.com>
2026-01-01 17:21:10 -0500

    add makeLevel function

--- munchieguardian2/src/scenes/floor1.js
+++ munchieguardian2/src/scenes/floor1.js
@@ -2,8 +2,28 @@ import kaplay from "kaplay";
 import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
 export function floor1Scene() {
-    setGravity(2700); //gravity = 3x jump force
+    setGravity(2400); //gravity = 3x jump force
     
+    makeLevel([
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "                              ",
+            "==============================",
+            "==============================",
+            "==============================",
+    ]);
+
     const munchieguardian = add([
         sprite("munchieguardian"),
         scale(0.25, 0.25),
@@ -17,13 +37,13 @@ export function floor1Scene() {
         "munchieguardian",
     ]);
 
-    const floor = add([
-        rect(1920, 200),
-        area(),
-        pos(0, height() - 200),
-        body({ isStatic: true }),
-        color(0, 20, 100),
-    ]);
+    // const floor = add([
+    //     rect(1920, 200),
+    //     area(),
+    //     pos(0, height() - 200),
+    //     body({ isStatic: true }),
+    //     color(0, 20, 100),
+    // ]);
 
     tween(
         vec2(10, 800),
@@ -33,24 +53,55 @@ export function floor1Scene() {
         easings.easeOutCirc
     );
 
+    var munchieguardianDirection = RIGHT;
+    let jumpsLeft = 2;
+
+    munchieguardian.onGround(() => {
+        jumpsLeft = 2;
+    }); //double jump stuff
+
     onKeyPress(["w", "up"], () => {
-        if (munchieguardian.isGrounded()) {
-            munchieguardian.jump(900);
+        if (jumpsLeft > 0) {
+            munchieguardian.jump(750);
+            jumpsLeft--;
         }
     })
-
 
     onKeyDown(["d", "right"], () => {
         munchieguardian.move(munchieguardian.speed, 0);
         munchieguardian.scale = vec2(0.25, 0.25);
+        munchieguardianDirection = RIGHT;
     })
 
     onKeyDown(["a", "left"], () => {
         munchieguardian.move(-munchieguardian.speed, 0);
         munchieguardian.scale = vec2(-0.25, 0.25);
-    }) 
+        munchieguardianDirection = LEFT;
+    }) //move controls
+    
+    onKeyPress("shift", () => {
+        if (munchieguardianDirection == RIGHT) {
+            tween(munchieguardian.pos.x, munchieguardian.pos.x + 200, 0.2, (v) => (munchieguardian.pos.x = v), easings.easeInOutCirc)
+            // munchieguardian.move(20*munchieguardian.speed, 15);
+        }
+        if (munchieguardianDirection == LEFT) {
+            tween(munchieguardian.pos.x, munchieguardian.pos.x - 200, 0.2, (v) => (munchieguardian.pos.x = v), easings.easeInOutCirc)
+        } //change direction
+    }) //dash
 
-    // munchieguardian.
-
+    function makeLevel(level) {
+        addLevel(level, {
+            tileWidth: 64,
+            tileHeight: 64,
+            tiles: {
+                "=": () => [
+                    rect(64, 64),
+                    area(),
+                    body({isStatic: true}),
+                    color(0, 10, 100),
+                ]
+            }
+        })
+    } 
 
 }
