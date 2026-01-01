d40344f3780f795973abcc0de8459a1ecfe944f0
kat <katwang7749@gmail.com>
2026-01-01 16:55:28 -0500

    add player movement controls

--- munchieguardian2/src/scenes/floor1.js
+++ munchieguardian2/src/scenes/floor1.js
@@ -2,33 +2,55 @@ import kaplay from "kaplay";
 import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
 export function floor1Scene() {
+    setGravity(2700); //gravity = 3x jump force
+    
     const munchieguardian = add([
         sprite("munchieguardian"),
         scale(0.25, 0.25),
         pos(10, 750), {
-            speed: 350,
+            speed: 500,
         },
         anchor("center"),
         area(),
         animate(),
+        body(),
         "munchieguardian",
     ]);
 
-    // munchieguardian.tween(vec2(10, 750), vec2(150, 750), 1, (value) => (munchieguardian.pos = value));    
-    // munchieguardian.animate("pos", [vec2(10, 750), vec2(150, 750)], {
-    //     duration: 0.5,
-    //     interpolation: "spline",
-    //     direction: "forward",
-    //     loop: false,
-    // })
-    
+    const floor = add([
+        rect(1920, 200),
+        area(),
+        pos(0, height() - 200),
+        body({ isStatic: true }),
+        color(0, 20, 100),
+    ]);
+
     tween(
-        vec2(10, 750),
-        vec2(200, 750),
+        vec2(10, 800),
+        vec2(200, 800),
         0.5,
         (v) => munchieguardian.pos = v,
-        (t) => (Math.sin((Math.PI * t) / 2))
+        easings.easeOutCirc
     );
+
+    onKeyPress(["w", "up"], () => {
+        if (munchieguardian.isGrounded()) {
+            munchieguardian.jump(900);
+        }
+    })
+
+
+    onKeyDown(["d", "right"], () => {
+        munchieguardian.move(munchieguardian.speed, 0);
+        munchieguardian.scale = vec2(0.25, 0.25);
+    })
+
+    onKeyDown(["a", "left"], () => {
+        munchieguardian.move(-munchieguardian.speed, 0);
+        munchieguardian.scale = vec2(-0.25, 0.25);
+    }) 
+
+    // munchieguardian.
 
 
 }
